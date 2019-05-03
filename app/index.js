abb0c8394d37d53a45196ec95335db398fa911ba
Thiago kauai <html5ephp@gmail.com>
2019-05-03 00:26:43 -0300

    adicionando e implementando ejs

--- app/index.js
+++ app/index.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express()
 const axios = require('axios')
 const url = 'https://gist.githubusercontent.com/bradtraversy/20dee7787486d10db3bd1f55fae5fdf4/raw/2c06c44dcea55ecbb6fbf20edfd240ec6373b688/state_capitals.json'
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs')  
+
 app.get('/', async (req,res) => {
    const result = await (await axios.get(url)).data
    console.log(result)
-   res.send(result)
+   res.render('home')
 })
 
 app.listen(3000,(e) => {
